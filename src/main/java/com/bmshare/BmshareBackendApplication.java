a87dfd8c0792fbc417118891511b885c31843395
Petros_K <soung0911@gmail.com>
2019-04-03 09:41:45 +0900

    boot+gradle+swagger

---
+++ src/main/java/com/bmshare/BmshareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bmshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmshareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmshareBackendApplication.class, args);
+	}
+
+}
